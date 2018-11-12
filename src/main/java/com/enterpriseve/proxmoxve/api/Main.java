1b91bb3180b2f92edd4b91bc7fe06de651ecf1d7
piszyna <a.piszyna@gmail.com>
2018-11-12 16:34:18 +0100

    Ask the user for his username and password

--- src/main/java/com/enterpriseve/proxmoxve/api/Main.java
+++ src/main/java/com/enterpriseve/proxmoxve/api/Main.java
@@ -1,20 +1,22 @@
 package com.enterpriseve.proxmoxve.api;
 
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.util.Map;
-
-import static com.enterpriseve.proxmoxve.api.Secret.PASSWORD;
-import static javax.swing.UIManager.get;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class Main {
+    public static String USERNAME;
+    public static String PASSWORD;
 
     public static void main(String[] args) throws JSONException {
         Client client = new Client("srv-px1.insa-toulouse.fr",8006);
 
-        client.login("piszyna", PASSWORD,"Ldap-INSA");
+        new Main().askUserForHisLogin();
+        client.login(USERNAME, PASSWORD,"Ldap-INSA");
 
 
         System.out.println("Information about the server srv-px5:");
@@ -43,5 +45,35 @@ public class Main {
         double ramUsagePercentage = memoryUsedSpace/memoryTotalSpace*100;
         System.out.println("ram usage percentage: "+ramUsagePercentage);
         System.out.println();
+    }
+
+
+
+
+    //ask the user for his usernale and password
+    public void askUserForHisLogin(){
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Enter your username: ");
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            USERNAME = br.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Enter your password: ");
+        try {
+            PASSWORD = br.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        reader.close();
+
+        //Print some stuff to hide the password
+        System.out.println("");
+        for (int i = 0;i<50;i++){
+            System.out.println("************");
+        }
+        System.out.println("");
     }
 }
--- src/main/java/com/enterpriseve/proxmoxve/api/Secret.java
+++
@@ -1,5 +0,0 @@
-package com.enterpriseve.proxmoxve.api;
-
-public class Secret {
-    public static String PASSWORD = "Binaire0101";
-}
